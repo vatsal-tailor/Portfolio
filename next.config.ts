3a47cec66b798f15215b0f0e6979edc1805c46e7
Vatsaltailo <vatsaltailor2306@gmail.com>
2026-06-29 01:04:35 +0530

    Deploying Portfolio

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   output: 'export', // Tells Next.js to generate static HTML/CSS/JS files
+    images: {
+      unoptimized: true, // Required because GitHub Pages doesn't support Next.js dynamic image optimization
+    },
+    basePath: '/Portfolio',
 };
 
 export default nextConfig;
